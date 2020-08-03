c5248cfa29497732d584341133d3a4a603e1af42
John Li <hud_johnli@live.co.uk>
2020-08-03 20:23:44 +0100

    Created base web pages (index, exercise, motivation, nutrition.php)

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+ini_set("session.save_path", "/home/unn_w16010421/sessionData");
+session_start();
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Stylesheets files (Latest compiled and minified Bootstrap 4 CSS, Google Fonts CSS, Index CSS) -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins&display=swap">
+    <link rel="stylesheet" href="assets/stylesheets/main.css">
+
+    <title>Home</title>
+</head>
+
+<body>
+
+    <!-- Script files(jQuery library, Popper JS, Latest Compiled Bootstrap JS, FontAwesome JS)-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <script src="https://kit.fontawesome.com/6cc49d804e.js" crossorigin="anonymous"></script>
+    <script src="assets/scripts/scripts.js"></script>
+</body>
+
+</html>
