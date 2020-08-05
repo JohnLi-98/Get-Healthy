12bc0620f16401053537691335f230be348f4bdf
John Li <hud_johnli@live.co.uk>
2020-08-05 20:19:15 +0100

    Worked more on navbar - trying to make the underline the length of the text only for each link on small screens

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ session_start();
     <!-- Stylesheets files (Latest compiled and minified Bootstrap 4 CSS, Google Fonts CSS, Index CSS) -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins&display=swap">
-    <link rel="stylesheet" href="assets/stylesheets/main.css">
+    <link rel="stylesheet" href="stylesheets/main.css">
 
     <title>Home</title>
 </head>
@@ -22,7 +22,12 @@ session_start();
 <body>
     <?php
     include "common/nav-unauth.html";
+    include "common/nav-auth.php";
     ?>
+
+    <div id="test">
+
+    </div>
 
     <!-- Script files(jQuery library, Popper JS, Latest Compiled Bootstrap JS, FontAwesome JS)-->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
