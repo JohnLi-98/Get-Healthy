967422d8001cbc63ea8140cb235afc32bc081917
John Li <hud_johnli@live.co.uk>
2020-08-06 20:18:11 +0100

    Completed static navbar functionality

--- index.php
+++ index.php
@@ -25,6 +25,16 @@ session_start();
     //include "common/nav-auth.php";
     ?>
 
+    <!-- Jumbotron that fills the height of the screen -->
+    <div class="jumbotron jumbotron-fluid" id="indexJumbotron">
+        <div class="row align-items-center mx-auto h-75 w-100">
+            <div class="col text-center">
+                <p>Welcome to</p>
+                <h1>GET HEALTHY</h1>
+            </div>
+        </div>
+    </div>
+
     <div id="test">
 
     </div>
