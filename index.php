7b7dc07dc63fd8fd40a943ed80bc24f71e33ef3d
John Li <hud_johnli@live.co.uk>
2020-08-15 15:31:18 +0100

    changed some of the padding for footer, added some content to index

--- index.php
+++ index.php
@@ -44,6 +44,88 @@ session_start();
         </div>
     </div>
 
+    <div class="container-fluid page-content">
+
+    </div>
+
+    <div class="container-fluid parallax" id="explore"></div>
+
+    <div class="container-fluid page-content">
+        <div class="row py-4 py-md-5">
+            <div class="col-12 col-md-6 my-auto pb-3">
+                <h3 class="card-title"><u>Exercise</u></h3>
+                <p class="card-text">
+                    Physical activity of moderate intensity - such as walking, cycling or doing
+                    other sports has significant benefits for health. At all ages, the benefits of being
+                    physically active outweigh potential harm, i.e. accidents and injuries. Some exercise
+                    is better than doing none. By becoming more active throuhout the day in
+                    relatively simple ways, people can easily achieve the recommended activity levels.
+                </p>
+                <button type="button" class="btn info-btn" style="border-radius:40px">
+                    <span>
+                        Find out more
+                        <i class="fas fa-angle-double-right"></i>
+                    </span>
+                </button>
+            </div>
+
+            <div class="col-12 col-md-6">
+                <img src="images/Exercise-Home-Image.jpg" class="card-img-top img-fluid">
+            </div>
+        </div>
+
+        <hr class="line-divider" />
+
+        <div class="row py-4 py-md-5">
+            <div class="col-12 col-md-6 order-md-2 my-auto pb-3">
+                <h3 class="card-title"><u>Motivation</u></h3>
+                <p class="card-text">
+                    If you want to accomplish your ambitions, you need to stop asking for permission. This
+                    is why motivation is important in life because it stops asking questions and aligns you
+                    to work towards your goals. Not everyone is born with motivation and it is the defining
+                    factor that turns a good thought into immediate action. It turns a good idea into a
+                    business and can positively impact the world around you.
+                </p>
+                <button type="button" class="btn info-btn" style="border-radius:40px">
+                    <span>
+                        Find out more
+                        <i class="fas fa-angle-double-right"></i>
+                    </span>
+                </button>
+            </div>
+
+            <div class="col-12 col-md-6 order-md-1">
+                <img src="images/Motivation-Home-Image.jpg" class="card-img-top img-fluid">
+            </div>
+        </div>
+
+        <hr class="line-divider" />
+
+        <div class="row py-4 py-md-5">
+            <div class="col-12 col-md-6 my-auto pb-3">
+                <h3 class="card-title"><u>Nutrition</u></h3>
+                <p class="card-text">
+                    Many people often underestimate the importance of the right food and nutrition has on
+                    our health. There are multiple multiple benefits that eating an abundance of different
+                    nutrients has on your body. Swapping high carbohydrates, sugars and fatty foods for whole
+                    nutritious foods will have a significant effect on your body, health and mood. A lot of
+                    people plan and start their healthy eating journey, but are unable to sustain these
+                    changes.
+                </p>
+                <button type="button" class="btn info-btn" style="border-radius:40px">
+                    <span>
+                        Find out more
+                        <i class="fas fa-angle-double-right"></i>
+                    </span>
+                </button>
+            </div>
+
+            <div class="col-12 col-md-6">
+                <img src="images/Nutrition-Home-Image.jpg" class="card-img-top img-fluid">
+            </div>
+        </div>
+    </div>
+
 
     <div class="container-fluid" id="test">
 
