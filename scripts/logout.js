f82a34a631d4a61ca5ee5ff1ba9f6550040d8c64
John Li <hud_johnli@live.co.uk>
2020-08-26 01:39:22 +0100

    made seperate js files for each page functions that are imported into main.js - need to figure out general functions as logout.js does not see createElement as defined

---
+++ scripts/logout.js
@@ -0,0 +1,84 @@
+/**
+ * Function to run when the logout button is clicked. Runs createLogoutMessage() to display a div before running
+ * logout.php through ajax. Upon success, show response then redirect to home page after 3 secs.
+ */
+export function logoutClick() {
+  $("#logout-button").click(function () {
+    createLogoutMessage();
+    setTimeout(function () {
+      $.ajax({
+        url: "ajax/logout.php",
+        success: function () {
+          $("#loading-icon, #success-response").toggleClass("d-none");
+          setTimeout(function () {
+            window.location.replace(
+              "http://unn-w16010421.newnumyspace.co.uk/Projects/Get-Healthy/index.php"
+            );
+          }, 3000);
+        },
+      });
+    }, 2000);
+  });
+}
+
+/**
+ * Function to create the logout loader to display confirmation message to the DOM. Use's bootstrap classes for divs.
+ */
+export function createLogoutMessage() {
+  const div = createElement("div", {
+    class: "container-fluid h-100 w-100 form-submit",
+    id: "loader",
+  });
+  const row = createElement("div", {
+    class: "row h-100",
+  });
+  const col = createElement("div", {
+    class:
+      "col-9 col-md-7 col-lg-5 justify-content-center align-items-center h-75 text-dark d-flex flex-column m-auto",
+  });
+  const iconDiv = createElement("div", {
+    class: "response text-center text-white",
+    id: "loading-icon",
+  });
+  const icon = createElement("i", {
+    class: "fas fa-spinner fa-pulse fa-4x",
+  });
+  const h2 = createElement("h2", {
+    class: "pt-4",
+  });
+  h2.append("Logging Out");
+  const responseDiv = createElement("div", {
+    class: "response alert-success text-center py-5 d-none",
+    id: "success-response",
+  });
+  const responseText = createElement("div", {
+    class: "mt-4 mx-4",
+  });
+  const h2Response = createElement("h2", {});
+  h2Response.append("Logged Out");
+  const pResponse = createElement("p", {});
+  pResponse.append("You have successfully logged out of your account.");
+  document.body
+    .appendChild(div)
+    .appendChild(row)
+    .appendChild(col)
+    .append(iconDiv, responseDiv);
+  iconDiv.append(icon, h2);
+  responseDiv.appendChild(responseText).append(h2Response, pResponse);
+}
+
+// Function to create an element and setting the attributes for it
+export function createElement(elemName, elemAttributes) {
+  // Create document element
+  var elem = document.createElement(elemName);
+  // Check if element attirbutes were passed
+  if (elemAttributes !== "undefined") {
+    // Loop through each argument
+    $.each(elemAttributes, function (key, value) {
+      // and assign it to the document element
+      elem.setAttribute(key, value);
+    });
+  }
+  // Return document element
+  return elem;
+}
---
+++ scripts/register.js
@@ -0,0 +1,250 @@
+/**
+ * Function to check that the email entered is valid. First checks whether the value entered returns true from
+ * validateEmail(), which is then passed as a post type for email-check.php through ajax to query the database to
+ * see if it already exists. The appropriate styling is displayed to the user, depending on the result.
+ */
+export function checkEmailValid() {
+  const emailVal = $("#register-email").val();
+  const fieldId = $("#register-email").attr("id");
+
+  if (validateEmail(emailVal)) {
+    $.ajax({
+      type: "post",
+      url: "../ajax/register/email-check.php",
+      data: {
+        email: emailVal,
+      },
+      success: function (data) {
+        if (data == "1") {
+          fieldInputInvalid(fieldId);
+          $("#email-check").html("Email already exists").css("color", "red");
+        } else {
+          fieldInputValid(fieldId);
+          $("#email-check").empty();
+        }
+      },
+    });
+  } else {
+    fieldInputInvalid(fieldId);
+    $("#email-check").html("Enter a Valid Email").css("color", "red");
+  }
+}
+
+/**
+ * Function that takes the emailAddress parameter and checks it with a regular expression for a valid email.
+ */
+export function validateEmail(emailAddress) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(emailAddress).toLowerCase());
+}
+
+/**
+ * Function to check that the username entered is valid. Checks whether the value entered is empty, more than 32
+ * or less than 6 characters to apply the invalid styling if so. If the value meets these conditions, then check
+ * whether username already exists in the database. If not, display to user that it is valid.
+ */
+export function checkUsernameValid() {
+  const usernameVal = $("#register-username").val();
+  const fieldId = $("#register-username").attr("id");
+
+  if (usernameVal === "") {
+    fieldInputInvalid(fieldId);
+    $("#username-check").html("Enter a Username").css("color", "red");
+  } else if (usernameVal.length > 32) {
+    fieldInputInvalid(fieldId);
+    $("#username-check").html("Too Long").css("color", "red");
+  } else if (usernameVal.length < 6) {
+    fieldInputInvalid(fieldId);
+    $("#username-check").html("Too Short").css("color", "red");
+  } else {
+    $.ajax({
+      type: "post",
+      url: "../ajax/register/username-check.php",
+      data: {
+        username: usernameVal,
+      },
+      success: function (data) {
+        if (data == "1") {
+          fieldInputInvalid(fieldId);
+          $("#username-check").html("Username taken").css("color", "red");
+        } else {
+          fieldInputValid(fieldId);
+          $("#username-check").empty();
+        }
+      },
+    });
+  }
+}
+
+/**
+ * Function to check that the passwords entered are matching. Like the other functions for validation, applies the
+ * appropriate styling to these fields, depending on the result.
+ */
+export function checkPwdMatch() {
+  const pwdVal = $("#register-pwd").val();
+  const pwdCVal = $("#register-pwdC").val();
+  const pwdId = $("#register-pwd").attr("id");
+  const pwdCId = $("#register-pwdC").attr("id");
+
+  if (
+    (pwdVal === "" && pwdCVal === "") ||
+    pwdVal.length < 8 ||
+    pwdCVal.length < 8
+  ) {
+    $("#register-pwd, #register-pwdC").removeClass("is-valid is-invalid");
+    $("#register-pwd, #register-pwdC").removeAttr("style");
+    $("#register-pwd, #register-pwdC").siblings(".fas").removeAttr("style");
+    $("#password-check").empty();
+  } else if (
+    pwdVal === pwdCVal &&
+    (pwdVal.length >= 8 || pwdCVal.length >= 8)
+  ) {
+    fieldInputValid(pwdId);
+    fieldInputValid(pwdCId);
+    $("#password-check").html("Matching").css("color", "green");
+  } else {
+    fieldInputInvalid(pwdId);
+    fieldInputInvalid(pwdCId);
+    $("#password-check").html("Not Matching").css("color", "red");
+  }
+}
+
+/**
+ * Function that changes the styling of the input field passed in as a parameter to a valid one.
+ */
+export function fieldInputValid(inputField) {
+  $("#" + inputField).removeClass("is-invalid");
+  $("#" + inputField).addClass("is-valid");
+  $("#" + inputField).attr("style", "border-color: #21d192");
+  $("#" + inputField)
+    .siblings(".fas")
+    .attr("style", "color: #21d192");
+}
+
+/**
+ * Function that changes the styling of the input field passed in as a parameter to a invalid one.
+ */
+export function fieldInputInvalid(inputField) {
+  $("#" + inputField).removeClass("is-valid");
+  $("#" + inputField).addClass("is-invalid");
+  $("#" + inputField).attr("style", "border-color: #ff0000");
+  $("#" + inputField)
+    .siblings(".fas")
+    .attr("style", "color: #ff0000");
+}
+
+/**
+ * Function to create an account with entered values once validated. Passes input field values to create-account.php
+ * file through ajax as post types. Upon success or error, response by toggling the class d-none for loading-icon
+ * and success-response/error-response. If successful, user is redirected to home page after 8 secs.
+ */
+export function createAccount() {
+  console.log("creating the account");
+  const emailVal = $("#register-email").val();
+  const usernameVal = $("#register-username").val();
+  const pwdVal = $("#register-pwd").val();
+  const pwdCVal = $("#register-pwdC").val();
+
+  $.ajax({
+    type: "post",
+    url: "../ajax/register/create-account.php",
+    data: {
+      email: emailVal,
+      username: usernameVal,
+      password: pwdVal,
+      passwordConfirm: pwdCVal,
+    },
+    success: function () {
+      $("#loading-icon, #success-response").toggleClass("d-none");
+      setTimeout(function () {
+        window.location.replace(
+          "http://unn-w16010421.newnumyspace.co.uk/Projects/Get-Healthy/index.php"
+        );
+      }, 8000);
+    },
+    error: function (res) {
+      $("#loading-icon, #error-response").toggleClass("d-none");
+      $("#error-message").html("An error occured: " + res.responseText);
+    },
+  });
+}
+
+/**
+ * Function to run checkEmailValid() when the register-email input field loses focus.
+ */
+export function emailFocusout() {
+  $("#register-email").on("focusout", function () {
+    checkEmailValid();
+  });
+}
+
+/**
+ * Function to run checkUsernameValid() when the register-username input field loses focus.
+ */
+export function usernameFocusout() {
+  $("#register-username").on("focusout", function () {
+    checkUsernameValid();
+  });
+}
+
+/**
+ * Function to run checkPwdMatch function when the register-pwd or register-pwdC input fields have a key entered.
+ */
+export function passwordKeyup() {
+  $("#register-pwd, #register-pwdC").keyup(checkPwdMatch);
+}
+
+/**
+ * Function to check form validation on submission. First, runs functions on email and username fields to apply
+ * appropriate styling. Then checks if the password fields don't have is-valid class, to apply is-invalid styling.
+ * Next, it checks each field input for is-invalid to set the formValid variable to the appropriate value. Finally,
+ * if formValid is not equal to false, toggle #loader to show and run createAccount() after 2 secs.
+ */
+export function registerSubmit() {
+  $("#register-form").on("submit", function (event) {
+    event.preventDefault();
+    event.stopPropagation();
+    let formValid;
+    checkEmailValid();
+    checkUsernameValid();
+    if (
+      !$("#register-pwd").hasClass("is-valid") ||
+      !$("#register-pwdC").hasClass("is-valid")
+    ) {
+      fieldInputInvalid($("#register-pwd").attr("id"));
+      fieldInputInvalid($("#register-pwdC").attr("id"));
+      if (
+        $("#register-pwd").val().length < 8 ||
+        $("#register-pwdC").val().length < 8
+      ) {
+        $("#password-check").html("Too Short").css("color", "red");
+      } else {
+        $("#password-check").html("Not Matching").css("color", "red");
+      }
+    }
+
+    $("#register-form input").each(function () {
+      if ($(this).hasClass("is-invalid")) {
+        formValid = false;
+      }
+    });
+
+    if (formValid !== false) {
+      $("#loader").toggleClass("d-none");
+      $(document.body).toggleClass("body-scroll");
+      setTimeout(function () {
+        createAccount();
+      }, 2000);
+    }
+  });
+}
+
+/**
+ * Function to close the #loader div when clicked to close.
+ */
+export function closeLoader() {
+  $(".close-loader").click(function () {
+    $("#loader, #error-response, #loading-icon").toggleClass("d-none");
+    $(document.body).toggleClass("body-scroll");
+  });
+}
