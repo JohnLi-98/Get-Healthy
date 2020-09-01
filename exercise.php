e40c39fd7dff057c138b3fba59ce1628df38e61a
John Li <hud_johnli@live.co.uk>
2020-09-01 20:36:45 +0100

    Added the basic markup of exercise and motivation.php to master branch

--- exercise.php
+++ exercise.php
@@ -1,5 +1,5 @@
 <?php
-ini_set("session.save_path", "/home/unn_w16010421/sessionData");
+//ini_set("session.save_path", "/home/unn_w16010421/sessionData");
 session_start();
 ?>
 
@@ -26,16 +26,36 @@ session_start();
     //include "common/nav-auth.php";
     ?>
 
+    <!-- Jumbotron that fills the height of the screen -->
+    <div class="jumbotron jumbotron-fluid p-0" id="exercise-jumbotron">
+        <div class="row align-items-center h-100 mx-5">
+            <div class="col">
+                <span><a href="index.php">Home</a>
+                    <span class="px-2">
+                        &#8594;
+                    </span>
+                    Exercise
+                </span>
+                <h1 class="pt-3"><strong>Exercise</strong></h1>
+                <p class="text-left">Every day is another chance to do something your future self will thank you for.</p>
+            </div>
+        </div>
+    </div>
+
     <div class="container-fluid text-center align-items-center" id="test">
         <h1>If you're reading this, you're gay</h1>
     </div>
+
+    <?php
+    include "common/footer.html";
+    ?>
 
     <!-- Script files(jQuery library, Popper JS, Latest Compiled Bootstrap JS, FontAwesome JS)-->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
     <script src="https://kit.fontawesome.com/6cc49d804e.js" crossorigin="anonymous"></script>
-    <script src="scripts/main.js"></script>
+    <script type="module" src="scripts/main.js"></script>
 </body>
 
 </html>
